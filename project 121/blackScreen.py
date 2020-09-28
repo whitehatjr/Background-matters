575450740d321f5384997ca78e68ddfe13c94d1a
whitehatjr <whitehatjr@whitehatjr.com>
2020-09-28 12:02:45 +0530

    C121 project solution

---
+++ project 121/blackScreen.py
@@ -0,0 +1,31 @@
+import cv2  
+import numpy as np  
+  
+video = cv2.VideoCapture(0) 
+image = cv2.imread("me.jpeg") 
+  
+while True: 
+  
+    ret, frame = video.read() 
+    print(frame)
+    frame = cv2.resize(frame, (640, 480)) 
+    image = cv2.resize(image, (640, 480)) 
+  
+  
+    u_black = np.array([104, 153, 70]) 
+    l_black = np.array([30, 30, 0]) 
+  
+    mask = cv2.inRange(frame, l_black, u_black) 
+    res = cv2.bitwise_and(frame, frame, mask = mask) 
+  
+    f = frame - res 
+    f = np.where(f == 0, image, f) 
+  
+    cv2.imshow("video", frame) 
+    cv2.imshow("mask", f) 
+  
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        break 
+  
+video.release() 
+cv2.destroyAllWindows() 
